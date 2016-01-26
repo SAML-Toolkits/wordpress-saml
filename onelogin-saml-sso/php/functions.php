56ba514dc54db7d061817dca7bcda2453eb98d0d
Sixto Martin <pitbulk@gmail.com>
2016-01-26 17:00:18 +0100

    Uncomment out filter based custom role code

--- onelogin-saml-sso/php/functions.php
+++ onelogin-saml-sso/php/functions.php
@@ -218,21 +218,21 @@ function saml_acs() {
 				add_filter('onelogin_custom_roles', 'add_custom_rolemapping'); 
 			*/
 
-			/*
-				if (has_filter('onelogin_custom_roles')) {
-					$customRoles = array();
-					$customRoles = apply_filters('onelogin_custom_roles', $customRoles);
-					$customRoles = array_unique($customRoles);
 
-					foreach ($attrs[$roleMapping] as $samlRole) {
-						if (in_array($samlRole, $customRoles) && $GLOBALS['wp_roles']->is_role( $samlRole)) {
-							$userdata['role'] = $samlRole;
-							$foundCustomizedRole = true; 
-							break; 
-						}
+			if (has_filter('onelogin_custom_roles')) {
+				$customRoles = array();
+				$customRoles = apply_filters('onelogin_custom_roles', $customRoles);
+				$customRoles = array_unique($customRoles);
+
+				foreach ($attrs[$roleMapping] as $samlRole) {
+					if (in_array($samlRole, $customRoles) && $GLOBALS['wp_roles']->is_role( $samlRole)) {
+						$userdata['role'] = $samlRole;
+						$foundCustomizedRole = true; 
+						break; 
 					}
-    			}
-			*/
+				}
+			}
+
 
 			if (!$foundCustomizedRole) {
 				$role = 0;
