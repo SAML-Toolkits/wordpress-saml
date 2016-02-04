662bb4029335ceed0115e614c941a7dd36356a02
Sixto Martin <pitbulk@gmail.com>
2016-02-04 12:27:34 +0100

    Fix #16. SAML Login link

--- onelogin-saml-sso/php/functions.php
+++ onelogin-saml-sso/php/functions.php
@@ -20,7 +20,11 @@ function saml_checker() {
 }
 
 function saml_custom_login_footer() {
-	$saml_login_message = get_option('onelogin_saml_customize_links_saml_login', "SAML Login");
+	$saml_login_message = get_option('onelogin_saml_customize_links_saml_login');
+	if (empty($saml_login_message)) {
+		$saml_login_message = "SAML Login";
+	}
+
     echo '<div style="font-size: 110%;padding:8px;background: #fff;text-align: center;"><a href="'.get_site_url().'/wp-login.php?saml_sso">'.$saml_login_message.'</a></div>';
 }
 
