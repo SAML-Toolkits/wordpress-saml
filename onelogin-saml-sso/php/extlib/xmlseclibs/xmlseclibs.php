95af776ab1bfa1775e584782d82f6ea68f11b919
Sixto Martin <pitbulk@gmail.com>
2019-12-04 17:29:55 +0100

    Fix path

--- onelogin-saml-sso/php/extlib/xmlseclibs/xmlseclibs.php
+++ onelogin-saml-sso/php/extlib/xmlseclibs/xmlseclibs.php
@@ -40,7 +40,7 @@
  * @version   3.0.4
  */
 
-$xmlseclibs_srcdir = dirname(__FILE__) . '/src/';
+$xmlseclibs_srcdir = dirname(__FILE__) . '/src';
 require $xmlseclibs_srcdir . '/XMLSecurityKey.php';
 require $xmlseclibs_srcdir . '/XMLSecurityDSig.php';
 require $xmlseclibs_srcdir . '/XMLSecEnc.php';
